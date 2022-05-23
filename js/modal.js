dc7c9bd51c94253c2886d073ac97ecba8cb1e492
Timofey Kremko <timkremko@gmail.com>
2022-05-23 16:07:44 +0300

    add: validation

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,31 @@
 const sendBtns = document.querySelectorAll(".send-btn");
+const inputName = document.querySelector(".input__name");
+const inputEmail = document.querySelector(".input__email");
+const inputPhone = document.querySelector(".input__phone");
+const regEmail =
+  /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+const regNumber = /^\+375\([0-9]{2}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/;
 
 sendBtns.forEach((item) => {
   item.addEventListener("click", () => {
-    Swal.fire("Форма отправлена", "Скоро с вами свяжутся", "success");
+    if (
+      inputName.value &&
+      inputEmail.value &&
+      inputPhone.value &&
+      regEmail.test(inputEmail.value) === true &&
+      regNumber.test(inputPhone.value) === true
+    ) {
+      Swal.fire("Форма отправлена", "Скоро с вами свяжутся", "success");
+    } else {
+      Swal.fire(
+        "Заполните форму",
+        "Для отрпавки формы заполните данные",
+        "error"
+      );
+    }
   });
 });
+
+// const houseName = document.querySelector(".house-form__name");
+// const housePhone = document.querySelector(".house-form__phone");
+// const formBtn = document.querySelector(".house__btn");
